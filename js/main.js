de66336458b2a51968275cfa0006938372fb372b
Ryan Edwards <ryanedwards@Ryans-MacBook-Air.local>
2017-06-06 11:36:43 -0400

    created object game logic

--- js/main.js
+++ js/main.js
@@ -34,7 +34,30 @@ function enoughCards(cards, card) {
 	return cardCount === 2;
 }
 
-var cards = populateCards(4);
+//var cards = populateCards(4);
+var cards = [
+	{
+		rank: 'queen',
+		suit: 'hearts',
+		cardImage: 'images/queen-of-hearts.png'
+	},
+	{
+		rank: 'queen',
+		suit: 'diamonds',
+		cardImage: 'images/queen-of-diamonds.png'
+	},
+	{
+		rank: 'king',
+		suit: 'hearts',
+		cardImage: 'images/king-of-hearts.png'
+	},
+	{
+		rank: 'king',
+		suit: 'diamonds',
+		cardImage: 'images/king-of-diamonds.png'
+	}
+];
+
 var cardsInPlay = [];
 
 function checkForMatch() {
@@ -48,8 +71,10 @@ function checkForMatch() {
 }
 
 function flipCard(cardId) {
-	console.log('User flipped ' + cards[cardId]);
-	cardsInPlay.push(cards[cardId]);
+	console.log('User flipped ' + cards[cardId].rank);
+	cardsInPlay.push(cards[cardId].rank);
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
 	checkForMatch();
 }
 
